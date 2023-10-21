54bb057dc1d33f41cab0cceaa6054660014e5a55
T3chTayl0r <144863113+T3chTayl0r@users.noreply.github.com>
2023-10-21 19:41:40 -0400

    Added Max lines as global constant and defined number of lines function

--- Slot-Machine.py
+++ Slot-Machine.py
@@ -1,3 +1,6 @@
+MAX_LINES = 3
+
+
 def deposit():
     while True:
         amount = input("How much would you like to deposit?")
@@ -11,6 +14,22 @@ def deposit():
             print("Please enter a number.")
 
     return amount
+
+def get_number_of_lines():
+     while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            amount = int(lines)
+            if  1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number.")
+
+    return lines
+
+
 
 def main()
     balance = deposit()
